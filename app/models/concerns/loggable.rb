0eb39f9c10dac417a0d0207db041f236ccc44424
Russell Jennings <violentpurr@gmail.com>
2017-07-29 17:11:05 -0400

    [m] create loggable concern

---
+++ app/models/concerns/loggable.rb
@@ -0,0 +1,44 @@
+require 'active_support/concern'
+
+module Loggable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :activity_logs, as: :loggable
+    after_update :record_changes
+  end
+
+  # records any changes made to self
+  def record_changes
+    self.activity_logs.create(changes_hash: self.changes)
+  end
+
+  # Rolls back the object to the previous version. repeated calls will go further back,
+  # unraveling changes made along the way.
+  # or pass in an integer of steps to go back from, but reload object before
+  # switching from without steps to with to avoid unexpected changes being acquired
+  #
+  # Parameters:
+  #  steps(optional)  - Integer of steps to go back
+  #
+  # Returns:
+  # Modified self with previous values altered
+  def undo(steps=nil)
+    @steps ||= 0
+    @steps += 1 unless steps
+    reset_to self.activity_logs.last(steps || @steps).first
+  end
+
+  # Resets self to be the previous version stored in a given activity_log
+  #
+  # Parameters:
+  #  activity_log  - an ActivityLog object to rollback to
+  #
+  # Returns:
+  #  self with modified attributes
+  def reset_to(activity_log)
+    activity_log.changes_hash.except("updated_at").each {|k, v| self[k] = v.first}
+    self
+  end
+
+end
